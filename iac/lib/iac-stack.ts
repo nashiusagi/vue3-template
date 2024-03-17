13a93a023040d5bea6226d985a5a5b0d1b30bf29
nashiusagi <ritotech0226@gmail.com>
2024-03-17 17:18:53 +0900

    [add] S3とcloudfrontのIaC化

--- iac/lib/iac-stack.ts
+++ iac/lib/iac-stack.ts
@@ -1,16 +1,75 @@
-import * as cdk from 'aws-cdk-lib';
+import { Duration, RemovalPolicy, Stack, StackProps, aws_cloudfront, aws_cloudfront_origins, aws_iam, aws_s3, aws_s3_deployment } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class IacStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class IacStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const websiteBucket = new aws_s3.Bucket(this, "websiteBucket", {
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'IacQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const originAccessIdentity = new aws_cloudfront.OriginAccessIdentity(
+      this,
+      "OriginAccessIdentity", {
+        comment: "website-distribution-originAccessIdentity",
+      }
+    );
+
+    const webSiteBucketPolicyStatement = new aws_iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      effect: aws_iam.Effect.ALLOW,
+      principals: [
+        new aws_iam.CanonicalUserPrincipal(
+          originAccessIdentity.cloudFrontOriginAccessIdentityS3CanonicalUserId
+        ),
+      ],
+      resources: [`${websiteBucket.bucketArn}/*`]
+    });
+
+    websiteBucket.addToResourcePolicy(webSiteBucketPolicyStatement);
+
+    const distribution = new aws_cloudfront.Distribution(this, "distribution", {
+      comment: "website-distribution",
+      defaultRootObject: "index.html",
+      errorResponses: [
+        {
+          ttl: Duration.seconds(300),
+          httpStatus: 403,
+          responseHttpStatus: 403,
+          responsePagePath: "/error.html",
+        },
+        {
+          ttl: Duration.seconds(300),
+          httpStatus: 404,
+          responseHttpStatus: 404,
+          responsePagePath: "/error.html",
+        },
+      ],
+      defaultBehavior: {
+        allowedMethods: aws_cloudfront.AllowedMethods.ALLOW_GET_HEAD,
+        cachedMethods: aws_cloudfront.CachedMethods.CACHE_GET_HEAD,
+        cachePolicy: aws_cloudfront.CachePolicy.CACHING_OPTIMIZED,
+        viewerProtocolPolicy: aws_cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        origin: new aws_cloudfront_origins.S3Origin(websiteBucket, {
+          originAccessIdentity,
+        }),
+      },
+      priceClass: aws_cloudfront.PriceClass.PRICE_CLASS_ALL
+    });
+
+    new aws_s3_deployment.BucketDeployment(this, "WebsiteDeploy", {
+      sources: [
+        aws_s3_deployment.Source.asset("../app/dist"),
+        aws_s3_deployment.Source.data(
+          "/error.html",
+          "<html><body><h1>error</h1></body></html>"
+        ),
+        aws_s3_deployment.Source.data("/favicon.ico", ""),
+      ],
+      destinationBucket: websiteBucket,
+      distribution: distribution,
+      distributionPaths: ["/*"],
+    });
   }
 }
