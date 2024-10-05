82db92c46702370d48a51c5bd039582884f3cdd0
nashiusagi <ritotech0226@gmail.com>
2024-10-05 13:18:14 +0900

    [add] サンプルのコンポーネントテストを追加

---
+++ app/src/components/atoms/ErrorLavel/ErrorLabel.test.ts
@@ -0,0 +1,16 @@
+import { mount } from "@vue/test-utils"
+import ErrorLabel from "./ErrorLabel.vue"
+import { describe, expect, test } from "vitest"
+
+describe("ErrorLabelのテスト", () => {
+  test("レンダリングのテスト", () => {
+    const wrapper = mount(ErrorLabel, {
+      props: {
+        error: "名前を入力してください"
+      }
+    })
+
+    expect(wrapper.find(".error_label").exists()).toBe(true)
+    expect(wrapper.find(".error_label").text()).toBe("名前を入力してください")
+  })
+})
