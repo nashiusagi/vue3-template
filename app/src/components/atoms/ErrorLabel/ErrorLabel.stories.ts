e52357f5b3f124180c778bbaa443e27c74879673
nashiusagi <ritotech0226@gmail.com>
2024-10-05 20:04:56 +0900

    [add] storybook追加

---
+++ app/src/components/atoms/ErrorLabel/ErrorLabel.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import ErrorLabel from "./ErrorLabel.vue";
+
+type Story = StoryObj<typeof ErrorLabel>;
+
+const meta: Meta<typeof ErrorLabel> = {
+  title: "ErrorLabel",
+  component: ErrorLabel,
+};
+
+export const Default: Story = {
+  render: () => ({
+    components: { ErrorLabel },
+    template: "<ErrorLabel error='タイトルは必須です' />",
+  }),
+};
+
+export default meta;
