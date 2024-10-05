a36e3f0ff489d40929b7eb2fa31e6bb5f1427f3b
nashiusagi <ritotech0226@gmail.com>
2024-10-05 20:35:01 +0900

    [add] PostCardのstorybook

---
+++ app/src/components/organisms/PostCard/PostCard.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import PostCard from "./PostCard.vue";
+
+type Story = StoryObj<typeof PostCard>;
+
+const meta: Meta<typeof PostCard> = {
+  title: "organisms/PostCard",
+  component: PostCard,
+};
+
+export const Default: Story = {
+  args: {
+    post: {
+      title: "title",
+      body: "body",
+    },
+  },
+  render: (args) => ({
+    components: { PostCard },
+    setup() {
+      return { ...args };
+    },
+    template: '<PostCard :post="post" />',
+  }),
+};
+
+export default meta;
