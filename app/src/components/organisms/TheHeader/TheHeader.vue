f5633d051af2010829e5b0577d1b415ade99605d
nashiusagi <ritotech0226@gmail.com>
2024-10-06 14:25:10 +0900

    [add] Headerでダークモードやりとりができるように

--- app/src/components/organisms/TheHeader/TheHeader.vue
+++ app/src/components/organisms/TheHeader/TheHeader.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import DarkModeToggleButton from "@/components/atoms/DarkModeToggleButton/DarkModeToggleButton.vue";
 import HeaderItem from "@/components/atoms/HeaderItem/HeaderItem.vue";
 </script>
 
@@ -8,12 +9,17 @@ import HeaderItem from "@/components/atoms/HeaderItem/HeaderItem.vue";
       <div>
         <span :class="$style.headerTitle">Vue3 Template</span>
       </div>
-      <nav :class="$style.headerNav">
-        <HeaderItem :link="'/index'" :title="'index'" />
-        <HeaderItem :link="'/sample'" :title="'sample'" />
-        <HeaderItem :link="'/sample'" :title="'sample2'" />
-        <HeaderItem :link="'/sample'" :title="'sample3'" />
-      </nav>
+      <div :class="$style.header_right">
+        <nav :class="$style.headerNav">
+          <HeaderItem :link="'/index'" :title="'index'" />
+          <HeaderItem :link="'/sample'" :title="'sample'" />
+          <HeaderItem :link="'/sample'" :title="'sample2'" />
+          <HeaderItem :link="'/sample'" :title="'sample3'" />
+        </nav>
+        <div :class="$style.dark_mode_button_container">
+          <DarkModeToggleButton />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -38,11 +44,23 @@ import HeaderItem from "@/components/atoms/HeaderItem/HeaderItem.vue";
   font-size: 32px;
 }
 
+.header_right {
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  gap: 8px;
+}
+
 .headerNav {
   height: 100%;
   display: flex;
   flex-direction: row;
   border: 2px solid #222;
   border-radius: 4px;
+}
+
+.dark_mode_button_container {
+  margin-top: auto;
+  margin-bottom: auto;
 }
 </style>
