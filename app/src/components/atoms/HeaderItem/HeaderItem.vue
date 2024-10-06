4ab1494351e25105b99dfdb9a8167df9cd6e2558
nashiusagi <ritotech0226@gmail.com>
2024-10-06 11:59:11 +0900

    [update] 原則style scopedは使わない

--- app/src/components/atoms/HeaderItem/HeaderItem.vue
+++ app/src/components/atoms/HeaderItem/HeaderItem.vue
@@ -6,12 +6,12 @@ defineProps<{
 </script>
 
 <template>
-  <div class="header_container">
-    <a class="header_title" :href="link">{{ title }}</a>
+  <div :class="$style.header_container" class="header_container">
+    <a :class="$style.header_title" class="header_title" :href="link">{{ title }}</a>
   </div>
 </template>
 
-<style scoped>
+<style module>
 .header_container {
   width: 160px;
   cursor: pointer;
