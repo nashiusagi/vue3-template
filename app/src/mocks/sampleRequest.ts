94182de783fe8ae75b3baa25e4d049934235ef0e
nashiusagi <ritotech0226@gmail.com>
2024-10-26 10:28:57 +0900

    [update] 動作確認用にテストデータを追加

--- app/src/mocks/sampleRequest.ts
+++ app/src/mocks/sampleRequest.ts
@@ -16,6 +16,56 @@ export const sampleRequest = () => {
           title: "Post 2",
           body: "this is a sample body 2",
         },
+        {
+          id: 3,
+          title: "Post 3",
+          body: "this is a sample body 3",
+        },
+        {
+          id: 4,
+          title: "Post 4",
+          body: "this is a sample body 4",
+        },
+        {
+          id: 5,
+          title: "Post 5",
+          body: "this is a sample body 5",
+        },
+        {
+          id: 6,
+          title: "Post 6",
+          body: "this is a sample body 6",
+        },
+        {
+          id: 7,
+          title: "Post 7",
+          body: "this is a sample body 7",
+        },
+        {
+          id: 8,
+          title: "Post 8",
+          body: "this is a sample body 8",
+        },
+        {
+          id: 9,
+          title: "Post 9",
+          body: "this is a sample body 9",
+        },
+        {
+          id: 10,
+          title: "Post 10",
+          body: "this is a sample body 10",
+        },
+        {
+          id: 11,
+          title: "Post 11",
+          body: "this is a sample body 11",
+        },
+        {
+          id: 12,
+          title: "Post 12",
+          body: "this is a sample body 12",
+        },
       ]);
     }),
   ];
