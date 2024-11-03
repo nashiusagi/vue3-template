dc0685def27da77c66196a8194156fa9ab3dc86c
nashiusagi <ritotech0226@gmail.com>
2024-11-03 10:55:39 +0900

    [add] viewportをstorybookに追加

--- app/.storybook/preview.ts
+++ app/.storybook/preview.ts
@@ -1,7 +1,15 @@
 import {setup} from '@storybook/vue3'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 setup((app) => {
   app.use(ElementPlus)
 })
+
+export const parameters = {
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+    defaultViewport: 'responsive'
+  },
+}
