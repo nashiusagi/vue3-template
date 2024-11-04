3d900a4e91069a652c2cfb44d093faa629cac096
nashiusagi <nashiusagi@users.noreply.github.com>
2024-11-04 01:32:00 +0000

    deploy: 0e16179a37c119934bc18fd72dbd7652a2e8edb4

---
+++ storybook-static/assets/TheHeader.stories-gPcca9X0.js
@@ -0,0 +1,20 @@
+import{T as t}from"./TheHeader-BlTsh425.js";import"./vue.esm-bundler-Dlgilagi.js";import"./DarkModeToggleButton-DRMj5OiA.js";import"./index-Bx0x86YK.js";import"./index-CH9iHxW2.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./HeaderItem-mkyleVQB.js";import"./MenuButton-C8lx54Iy.js";const f={title:"organisms/TheHeader",component:t},e={render:()=>({components:{TheHeader:t},template:"<TheHeader />"})},r={render:()=>({components:{TheHeader:t},template:"<TheHeader />"}),parameters:{viewport:{defaultViewport:"iphonex"}}};var o,a,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      TheHeader
+    },
+    template: "<TheHeader />"
+  })
+}`,...(n=(a=e.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};var p,s,m;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      TheHeader
+    },
+    template: "<TheHeader />"
+  }),
+  parameters: {
+    viewport: {
+      defaultViewport: "iphonex"
+    }
+  }
+}`,...(m=(s=r.parameters)==null?void 0:s.docs)==null?void 0:m.source}}};const w=["Default","Mobile"];export{e as Default,r as Mobile,w as __namedExportsOrder,f as default};
