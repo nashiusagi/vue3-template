81ebe78996ff8c8eb7bf98cccc543818e22fb50b
nashiusagi <ritotech0226@gmail.com>
2024-11-09 22:48:04 +0900

    [update] StampCardにBG設定

--- app/src/components/organisms/StampCard/StampCard.vue
+++ app/src/components/organisms/StampCard/StampCard.vue
@@ -25,8 +25,9 @@ defineProps({
   flex-direction: row;
   flex-wrap: wrap;
   gap: 4px;
+  background-color: #c2baba;
 
-  border: 2px solid #000;
+  border: 2px solid #c2baba;
   padding: 16px;
   width: calc( 54px * 5 + 4px * 4 );
   height: calc( 54px * 2 + 4px );
