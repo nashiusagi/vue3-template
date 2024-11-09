b710009e670768c3bc3396695c047a1d0935f58b
nashiusagi <ritotech0226@gmail.com>
2024-11-09 20:36:53 +0900

    [add] CheckedStamp

---
+++ app/src/components/atoms/CheckedStamp/CheckedStamp.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { Check } from "@element-plus/icons-vue";
+</script>
+<template>
+  <div :class="$style.stamp">
+    <span :class="$style.stamp_text">
+      <el-icon size="32"><Check /></el-icon>
+    </span>
+  </div>
+</template>
+
+<style module>
+.stamp {
+  border-radius: 999px;
+  border: 3px solid #28ca28;
+  width: 48px;
+  height: 48px;
+  background-color: #28ca28;
+}
+
+.stamp_text {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+
+  color: #fff;
+}
+</style>
