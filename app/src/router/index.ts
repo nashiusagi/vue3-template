2f7933f03238d8520125ca83a6b95d5aebdd9dac
nashiusagi <ritotech0226@gmail.com>
2024-11-10 21:29:33 +0900

    [add] axios inceptor for login

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -4,6 +4,9 @@ import {
   type Router,
   type RouteRecordRaw,
 } from "vue-router";
+import _axios from "@/plugins/axios";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { storeToRefs } from "pinia";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -21,6 +24,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/post/create",
     component: () => import("@/pages/CreatePost.vue"),
+    meta: { requiredAuth: true },
   },
   {
     path: "/login",
@@ -31,4 +35,25 @@ const routes: Array<RouteRecordRaw> = [
 export const router: Router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach(async (to, _, next) => {
+  if (to.matched.some((record) => record.meta.requiredAuth)) {
+    /**
+     * NOTE: アクセストークンの有効性チェック
+     * 認証エラーになった場合はaxiosの共通処理で自動的にRefresh Tokenを使用してAccess Tokenを再取得する
+     * アクセストークンの再取得に失敗した場合はログアウト処理を行い、セッションタイムアウト画面へ自動的に遷移する
+     * catchに空の関数を渡す必要がある, ないとエラーになってしまう
+     */
+    await _axios.post("Authentication/token/verify").catch(() => {});
+
+    const authStore = useAuthStore();
+    const { isLoggedIn } = storeToRefs(authStore);
+
+    if (!isLoggedIn.value) {
+      next("/login");
+    }
+  }
+
+  next();
 });
