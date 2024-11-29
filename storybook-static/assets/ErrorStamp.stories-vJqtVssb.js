62e5ebdee087d97c9890445edeec42b4db1ddb65
nashiusagi <nashiusagi@users.noreply.github.com>
2024-11-29 13:40:05 +0000

    deploy: 62efc48d9186853eee947bc019c13faeea6073e5

---
+++ storybook-static/assets/ErrorStamp.stories-vJqtVssb.js
@@ -0,0 +1,8 @@
+import{E as a}from"./ErrorStamp-CPtfnhBZ.js";import"./vue.esm-bundler-CFI5x4Jl.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const s={title:"atoms/stamps/ErrorStamp",component:a},r={render:()=>({components:{ErrorStamp:a},template:"<ErrorStamp />"})};var t,e,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      ErrorStamp
+    },
+    template: "<ErrorStamp />"
+  })
+}`,...(o=(e=r.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const c=["Default"];export{r as Default,c as __namedExportsOrder,s as default};
---
+++ storybook-static/assets/TheHeader.stories-BJmoVxi5.js
@@ -0,0 +1,20 @@
+import{T as t}from"./TheHeader-D_nJv6Ay.js";import"./vue.esm-bundler-CFI5x4Jl.js";import"./DarkModeToggleButton-D7osm-qw.js";import"./index-BbPEOEYU.js";import"./index-IYICR-OX.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./HeaderItem-xjh5WTQw.js";import"./MenuButton-fDqxRJsO.js";const f={title:"organisms/TheHeader",component:t},e={render:()=>({components:{TheHeader:t},template:"<TheHeader />"})},r={render:()=>({components:{TheHeader:t},template:"<TheHeader />"}),parameters:{viewport:{defaultViewport:"iphonex"}}};var o,a,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      TheHeader
+    },
+    template: "<TheHeader />"
+  })
+}`,...(n=(a=e.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};var p,s,m;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      TheHeader
+    },
+    template: "<TheHeader />"
+  }),
+  parameters: {
+    viewport: {
+      defaultViewport: "iphonex"
+    }
+  }
+}`,...(m=(s=r.parameters)==null?void 0:s.docs)==null?void 0:m.source}}};const w=["Default","Mobile"];export{e as Default,r as Mobile,w as __namedExportsOrder,f as default};
