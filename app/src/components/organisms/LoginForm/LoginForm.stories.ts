f27abaf51d59c3046d86e994cb80a3fb9c5ed5e3
nashiusagi <ritotech0226@gmail.com>
2024-11-29 22:25:15 +0900

    [fix] storybookでpiniaを使う

--- app/src/components/organisms/LoginForm/LoginForm.stories.ts
+++ app/src/components/organisms/LoginForm/LoginForm.stories.ts
@@ -1,7 +1,14 @@
-import type { Meta, StoryObj } from "@storybook/vue3";
+import { setup, type Meta, type StoryObj } from "@storybook/vue3";
 import LoginForm from "./LoginForm.vue";
+import { createPinia } from "pinia";
+import type { App } from "vue";
 
 type Story = StoryObj<typeof LoginForm>;
+
+const pinia = createPinia();
+setup((app: App) => {
+  app.use(pinia)
+})
 
 const meta: Meta<typeof LoginForm> = {
   title: "organisms/LoginForm",
