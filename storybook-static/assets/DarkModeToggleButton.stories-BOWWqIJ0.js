3f626d813f1a0c4d45f2c5c40ba2ba7395463076
nashiusagi <nashiusagi@users.noreply.github.com>
2025-01-13 04:53:49 +0000

    deploy: a908c6c6a07cec9c9af9a70b2ad120d25f9778de

--- storybook-static/assets/DarkModeToggleButton.stories-BOWWqIJ0.js
+++
@@ -1,8 +0,0 @@
-import{D as a}from"./DarkModeToggleButton-D8E5M2HQ.js";import"./vue.esm-bundler-BDpnlfzR.js";import"./index-kBzaHthy.js";import"./index-B9Rwq-Lr.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const c={title:"atoms/DarkModeToggleButton",component:a},e={render:()=>({components:{DarkModeToggleButton:a},template:"<DarkModeToggleButton error='タイトルは必須です' />"})};var o,t,r;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: () => ({
-    components: {
-      DarkModeToggleButton
-    },
-    template: "<DarkModeToggleButton error='タイトルは必須です' />"
-  })
-}`,...(r=(t=e.parameters)==null?void 0:t.docs)==null?void 0:r.source}}};const l=["Default"];export{e as Default,l as __namedExportsOrder,c as default};
---
+++ storybook-static/assets/MenuButton.stories-JIsy90ca.js
@@ -0,0 +1,8 @@
+import{M as r}from"./MenuButton-DIysyd1V.js";import"./vue.esm-bundler-QVxwmv7O.js";import"./index-BlLUFM5G.js";import"./index-Dxp45zSq.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const c={title:"atoms/MenuButton",component:r},t={render:()=>({components:{MenuButton:r},template:"<MenuButton />"})};var e,n,o;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      MenuButton
+    },
+    template: "<MenuButton />"
+  })
+}`,...(o=(n=t.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};const d=["Default"];export{t as Default,d as __namedExportsOrder,c as default};
