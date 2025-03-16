c13b160b4a0013c1ba716aa9ce4b5edd39c857cd
nashiusagi <nashiusagi@users.noreply.github.com>
2025-03-16 23:35:07 +0000

    deploy: 83f97e4063a4380959a04b4a67d601c08615b0e3

---
+++ storybook-static/assets/ErrorLabel.stories-DwNa285M.js
@@ -0,0 +1,8 @@
+import{d as n,c as p,A as c,n as m,o as _}from"./vue.esm-bundler-BG6wH0F0.js";import{_ as u}from"./_plugin-vue_export-helper-DlAUqK2U.js";const t=n({__name:"ErrorLabel",props:{error:{}},setup(E){return(e,f)=>(_(),p("p",{class:m([e.$style.error_label,"error_label"])},c(e.error),3))}}),d="_error_label_1aqp5_2",i={error_label:d},b={$style:i},l=u(t,[["__cssModules",b]]);t.__docgenInfo={exportName:"default",displayName:"ErrorLabel",description:"",tags:{},props:[{name:"error",required:!0,type:{name:"string"}}],sourceFiles:["/home/runner/work/vue3-template/vue3-template/app/src/components/atoms/ErrorLabel/ErrorLabel.vue"]};const g={title:"atoms/ErrorLabel",component:l},r={render:()=>({components:{ErrorLabel:l},template:"<ErrorLabel error='タイトルは必須です' />"})};var o,a,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      ErrorLabel
+    },
+    template: "<ErrorLabel error='タイトルは必須です' />"
+  })
+}`,...(s=(a=r.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const k=["Default"];export{r as Default,k as __namedExportsOrder,g as default};
