d6045b2a85b8b5a99cd447048b5ef977f79119f6
nashiusagi <nashiusagi@users.noreply.github.com>
2025-11-14 10:40:04 +0000

    deploy: f04fa7ba22ebba5d72fa7afea23c764a4f7b9f57

---
+++ storybook-static/assets/DarkModeToggleButton.stories-Dwogbrft.js
@@ -0,0 +1,8 @@
+import{D as a}from"./DarkModeToggleButton-BD2irJHb.js";import"./vue.esm-bundler-DbAMlMm1.js";import"./index-A-jk3ZjS.js";import"./index-xPiGKbYu.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const c={title:"atoms/DarkModeToggleButton",component:a},e={render:()=>({components:{DarkModeToggleButton:a},template:"<DarkModeToggleButton error='タイトルは必須です' />"})};var o,t,r;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      DarkModeToggleButton
+    },
+    template: "<DarkModeToggleButton error='タイトルは必須です' />"
+  })
+}`,...(r=(t=e.parameters)==null?void 0:t.docs)==null?void 0:r.source}}};const l=["Default"];export{e as Default,l as __namedExportsOrder,c as default};
