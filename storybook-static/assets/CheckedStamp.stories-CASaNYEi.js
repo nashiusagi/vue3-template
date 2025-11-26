277de4f9dae8dd4a16417b599beab5d8546d41e3
nashiusagi <nashiusagi@users.noreply.github.com>
2025-11-26 09:42:09 +0000

    deploy: 6bdb9d271cceefc2689d2df38a21d399cb22c92d

---
+++ storybook-static/assets/CheckedStamp.stories-CASaNYEi.js
@@ -0,0 +1,8 @@
+import{C as o}from"./CheckedStamp-C_9FTr2J.js";import"./vue.esm-bundler-QRDY-WvT.js";import"./index-wbYPFVkP.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const c={title:"atoms/stamps/CheckedStamp",component:o},e={render:()=>({components:{CheckedStamp:o},template:"<CheckedStamp />"})};var t,a,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      CheckedStamp
+    },
+    template: "<CheckedStamp />"
+  })
+}`,...(r=(a=e.parameters)==null?void 0:a.docs)==null?void 0:r.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,c as default};
