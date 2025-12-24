814330e55503e4d42bfa3bd842e3a0252263a9be
nashiusagi <nashiusagi@users.noreply.github.com>
2025-12-24 09:07:56 +0000

    deploy: cdaad3997a4b0de51511fd531dbaa9c4a525a54b

---
+++ storybook-static/assets/MenuButton.stories-DTTBMpNl.js
@@ -0,0 +1,8 @@
+import{M as r}from"./MenuButton-CuzaTThn.js";import"./vue.esm-bundler-54rJ_3Pj.js";import"./index-BvPdX1Fl.js";import"./index-Cu_C--Os.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const c={title:"atoms/MenuButton",component:r},t={render:()=>({components:{MenuButton:r},template:"<MenuButton />"})};var e,n,o;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: () => ({
+    components: {
+      MenuButton
+    },
+    template: "<MenuButton />"
+  })
+}`,...(o=(n=t.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};const d=["Default"];export{t as Default,d as __namedExportsOrder,c as default};
