16dccac5007b3c8818aabcf5c0a45d7d80aa4ebb
nashiusagi <nashiusagi@users.noreply.github.com>
2026-02-05 04:23:48 +0000

    deploy: d9cc32f20483127ab48cb72682c5bfe79167a7af

--- storybook-static/assets/SideBar.stories-DqrXWSpf.js
+++
@@ -1,8 +0,0 @@
-import{S as o}from"./SideBar-BNssj3px.js";import"./vue.esm-bundler-BwU7cRng.js";import"./index-HJT6-hE5.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const d={title:"organisms/SideBar",component:o},e={render:()=>({components:{SideBar:o},template:"<SideBar />"})};var r,t,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  render: () => ({
-    components: {
-      SideBar
-    },
-    template: "<SideBar />"
-  })
-}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const i=["Default"];export{e as Default,i as __namedExportsOrder,d as default};
